c87aae2aef63b3e1f51934a247d4a30cf4e24328
Owen Rackham <owen.rackham@gmail.com>
2013-09-10 21:29:57 +0100

    added the changes that trev asked for

--- lib/BastideDeLaCroix/Home.pm
+++ lib/BastideDeLaCroix/Home.pm
@@ -20,6 +20,60 @@ sub contact {
     bg_img => '../img/bg_img.jpg');
 }
 
+sub bedroom {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The Bedrooms",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub bathroom {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The Bathrooms",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub living {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The Living Rooms",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub garden {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The Gardens",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub other {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "Other Information",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub dining {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The Dining Rooms",
+    bg_img => '../img/bg_img.jpg');
+}
+
 sub gallery {
   my $self = shift;
 
@@ -28,6 +82,25 @@ sub gallery {
     message => "Photo Gallery",
     bg_img => '../img/bg_img.jpg');
 }
+
+sub villa {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The villa facilities",
+    bg_img => '../img/bg_img.jpg');
+}
+
+sub area {
+  my $self = shift;
+
+  # Render template "home/welcome.html.ep" with message
+  $self->render(
+    message => "The surrounding area",
+    bg_img => '../img/bg_img.jpg');
+}
+
 
 sub hacks {
   my $self = shift;
